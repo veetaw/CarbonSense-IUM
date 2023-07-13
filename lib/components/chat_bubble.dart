001b5734534520cc4db8c78329d72e21cee4d21c
vito <vitopiegari01@gmail.com>
2023-07-13 10:47:36 +0200

    Completes implementation of chat bubbles

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,27 +1,146 @@
+import 'package:carbonsense/constants/quick_questions.dart';
+import 'package:carbonsense/theme/constants/colors.dart';
 import 'package:flutter/material.dart';
 
-class ChatBubble extends StatelessWidget {
-  final String text;
+enum ChatBubbleType { chatBubbleAvatarOnLeft, chatBubbleAvatarOnRight }
 
-  const ChatBubble({super.key, required this.text});
+class ChatBubble extends StatelessWidget {
+  final double kRoundBorderRadius = 16;
+  final double kSharpBorderRadius = 1;
+  final double kAvatarRadius = 16;
+  final double kOuterMarginRadius = 16;
+  final double kInternalMarginRadius = 16;
+
+  final String text;
+  final ChatBubbleType type;
+  final bool hasQuickQuestions;
+  final Function(String)? onQuickQuestionTap;
+
+  const ChatBubble({
+    super.key,
+    required this.text,
+    this.type = ChatBubbleType.chatBubbleAvatarOnLeft,
+    this.hasQuickQuestions = false,
+    this.onQuickQuestionTap,
+  });
+
+  RoundedRectangleBorder getBorders() => RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(kRoundBorderRadius),
+          topRight: Radius.circular(kRoundBorderRadius),
+          bottomLeft: Radius.circular(
+              type == ChatBubbleType.chatBubbleAvatarOnLeft
+                  ? kSharpBorderRadius
+                  : kRoundBorderRadius),
+          bottomRight: Radius.circular(
+              type == ChatBubbleType.chatBubbleAvatarOnLeft
+                  ? kRoundBorderRadius
+                  : kSharpBorderRadius),
+        ),
+      );
+
+  List<Widget> getAvatar() => [
+        if (type == ChatBubbleType.chatBubbleAvatarOnRight)
+          const Padding(
+            padding: EdgeInsets.only(right: 10),
+          ),
+        CircleAvatar(
+          backgroundColor: kLightGreen2,
+          radius: kAvatarRadius,
+          child: Icon(type == ChatBubbleType.chatBubbleAvatarOnLeft
+              ? Icons.youtube_searched_for
+              : Icons.person),
+        ),
+        if (type == ChatBubbleType.chatBubbleAvatarOnLeft)
+          const Padding(
+            padding: EdgeInsets.only(right: 10),
+          ),
+      ];
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.2,
-      child: Card(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(16),
-            topRight: Radius.circular(16),
-            bottomLeft: Radius.circular(1),
-            bottomRight: Radius.circular(16),
+    return Container(
+      margin: EdgeInsets.all(kOuterMarginRadius),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              if (type == ChatBubbleType.chatBubbleAvatarOnLeft) ...getAvatar(),
+              if (type == ChatBubbleType.chatBubbleAvatarOnRight)
+                const Spacer(),
+              Card(
+                elevation: 0,
+                color: kLightGreen4,
+                shape: getBorders(),
+                child: Padding(
+                  padding: EdgeInsets.all(kInternalMarginRadius),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * .6,
+                    child: Text(
+                      text,
+                      maxLines: 10,
+                    ),
+                  ),
+                ),
+              ),
+              if (type == ChatBubbleType.chatBubbleAvatarOnRight)
+                ...getAvatar(),
+            ],
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Text(text),
-        ),
+          if (hasQuickQuestions) ...[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 8,
+                left:
+                    kInternalMarginRadius + kOuterMarginRadius + kAvatarRadius,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "DOMANDE RAPIDE",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  ...kQuickQuestions
+                      .map(
+                        (e) => Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              kRoundBorderRadius / 2,
+                            ),
+                          ),
+                          elevation: 0,
+                          color: kLightGreen4,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(
+                              kRoundBorderRadius / 2,
+                            ),
+                            onTap: () {
+                              if (onQuickQuestionTap != null) {
+                                onQuickQuestionTap!(e);
+                              }
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                e,
+                                style: const TextStyle(
+                                  color: kDarkGreen,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ],
+              ),
+            ),
+          ]
+        ],
       ),
     );
   }
---
+++ lib/constants/quick_questions.dart
@@ -0,0 +1,6 @@
+final List<String> kQuickQuestions = [
+  "Come posso migliorare il mio stile di vita?",
+  "Quanto è inquinante la carne di allevamento?",
+  "Quanto è utile la raccolta differenziata?",
+  "Grazie."
+];
