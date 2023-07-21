48a8d7fae41eef706f3677ba2dd0819e611260f9
vito <vitopiegari01@gmail.com>
2023-07-22 01:04:12 +0200

    Add custom slider implementation

---
+++ lib/components/custom_slider.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomSlider extends StatefulWidget {
+  final double max;
+  final double min;
+  final double initialValue;
+  final EdgeInsets padding;
+  final bool round;
+  final int? steps;
+
+  const CustomSlider({
+    super.key,
+    required this.max,
+    this.min = 0,
+    this.initialValue = 0,
+    this.padding = EdgeInsets.zero,
+    this.round = false,
+    this.steps,
+  });
+
+  @override
+  State<CustomSlider> createState() => _CustomSliderState();
+}
+
+class _CustomSliderState extends State<CustomSlider> {
+  double? value;
+
+  @override
+  void initState() {
+    value ??= widget.initialValue;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: widget.padding,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text(
+            "${widget.min.round()}",
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Expanded(
+            child: Slider(
+              min: widget.min,
+              max: widget.max,
+              value: value!,
+              divisions: widget.steps,
+              onChanged: (v) {
+                setState(() => value = v);
+              },
+              label: widget.round
+                  ? value!.round().toString()
+                  : value!.toStringAsFixed(1),
+            ),
+          ),
+          Text(
+            "${widget.max.round()}",
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
